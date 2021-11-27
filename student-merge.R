78da9d6018855ad4a86e825420c14ba4e3e76284
José Arthur <53868368+josearthursouza@users.noreply.github.com>
2021-11-27 12:20:36 -0300

    Update student-merge.R

--- student-merge.R
+++ student-merge.R
@@ -1,26 +1,27 @@
 library(ggplot2)
+
+setwd("C:/Users/josea/FGV/aedv/aedv_trabA2")
 d2=read.csv("student-por.csv")
 
 #d2$Z <- runif(649, 0, 5)
 
-graf1 <- ggplot(d2, aes(x=absences, y=G3, colour=sex)) + geom_point()
+graf1 <- ggplot(d2, aes(x=absences, y=G3, colour=Pstatus)) + geom_point()
 print(graf1)
 
-graf11 <- ggplot(d2, aes(x=age, y=G3, colour=sex)) + geom_point()
-print(graf11)
-
-graf12 <- ggplot(d2, aes(x=age, y=absences, colour=sex)) + geom_point()
-print(graf12)
-#ggsave("fig1.png",device="png",dpi = 300)
-
-#graf2 <- ggplot(d2, aes( x=famrel, y=Dalc)) + geom_tile()
+graf2 <- ggplot(d2, aes(x=age, y=G3, colour=sex)) + geom_point()
 #print(graf2)
 
-#graf3 <- ggplot(d2, aes( x=goout, y=Dalc)) + geom_tile()
+graf3 <- ggplot(d2, aes(x=age, y=absences, colour=sex)) + geom_point()
 #print(graf3)
+#ggsave("fig1.png",device="png",dpi = 300)
 
+graf4 <- ggplot(d2, aes( x=famrel, y=Dalc)) + geom_count() #geom_jitter() #ou geom_count()
+print(graf4)
 
 # Grouped
-graf3 <- ggplot(d2, aes(fill=sex, y=absences, x=famrel)) + 
+graf5 <- ggplot(d2, aes(fill=sex, y=absences, x=famrel)) + 
   geom_bar(position="dodge", stat="identity")
-#print(graf3)
+print(graf5)
+
+graf6 <- ggplot(d2, aes(x=G3))+geom_bar()
+print(graf6)
